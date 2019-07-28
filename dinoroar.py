e9b6af5ed1b9cec9b5a6853b1fa6bc9ee8700ea8
Jason Zylks <jzylks@gmail.com>
2019-07-28 07:51:03 +0100

    - Adding .gitignore - Reorganizing dinoroar.py, moving code into new Dinoroar class - Removing VLC; need a lighter weight solution for the RPi 0, using mpg123 for now - Removing alsaaudio; running into very strange behavior with PulseAudio/Alsa (audio either muted or 100% with no control if pulseaudio running)   that seems to coincide with use of this package

--- dinoroar.py
+++ dinoroar.py
@@ -1,107 +1,102 @@
 import re
 import os
 import json
+from subprocess import Popen
 
 import RPi.GPIO as GPIO
 import toml
 from paho.mqtt.client import Client
-import vlc
 import random
-import alsaaudio
-
-DISABLE_MIC_AT_REST = False
-STORY_PATH = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'stories')
-PLAYER = vlc.MediaPlayer()
-STORIES = [os.path.join(STORY_PATH, story) for story in os.listdir(STORY_PATH)]
-with open('/etc/snips.toml') as f:
-    CONFIG = toml.load(f)
-with open('/usr/share/snips/assistant/assistant.json') as f:
-    MODEL = json.load(f)
-HOST = CONFIG['snips-common']['mqtt'].split(':')[0]
-SITE = CONFIG['snips-audio-server']['bind'].split('@')[0]
-CLIENT = Client()
-CLIENT.username_pw_set(CONFIG['snips-common']['mqtt_username'], CONFIG['snips-common']['mqtt_password'])
-if DISABLE_MIC_AT_REST:
-    mix = alsaaudio.Mixer('Capture')
 
 
-def disable_mic():
-    if DISABLE_MIC_AT_REST:
-        mix.setrec(0)
+class Dinoroar:
 
-
-def enable_mic():
-    if DISABLE_MIC_AT_REST:
-        mix.setrec(1)
-
-
-disable_mic()
-
-
-def on_connect(client, userdata, flags, rc):
-    client.subscribe("hermes/#")
+    action_sub = None
+    mqtt_client = None
     
+    def __init__(self):
+        story_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'stories')
+        assets_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'assets')
+        startup_sub = Popen(['mpg123', os.path.join(assets_path, 'lotus-by-kevin-macleod.mp3')])
+        self.stories = [os.path.join(story_path, story) for story in os.listdir(story_path)]
+        with open('/etc/snips.toml') as f:
+            self.snips_config = toml.load(f)
+        with open('/usr/share/snips/assistant/assistant.json') as f:
+            self.snips_model = json.load(f)
+        self.snips_site = self.snips_config['snips-audio-server']['bind'].split('@')[0]
+        self._initialize_mqtt()
+        self._initialize_gpio()
+        startup_sub.terminate()
 
-def on_message(client, userdata, message):
-    topic = message.topic
-    payload = message.payload
-    if topic == "hermes/asr/startListening":
-        listening_started()
-    elif topic == "hermes/asr/stopListening":
-        listening_stopped()
-    elif re.match("hermes/hotword/.+/detected", topic):
-        hotword_detected()
-    elif re.match("hermes/intent/.+", topic):
-        process_intent(json.loads(payload))
-    elif topic.startswith("hermes/asr"):
-        print(topic)
-    else:
-        print(topic)
+    def _initialize_mqtt(self):
+        host = self.snips_config['snips-common']['mqtt'].split(':')[0]
+        self.mqtt_client = Client()
+        self.mqtt_client.username_pw_set(self.snips_config['snips-common']['mqtt_username'], self.snips_config['snips-common']['mqtt_password'])
+        self.mqtt_client.on_connect = self.mqtt_on_connect
+        self.mqtt_client.on_message = self.mqtt_on_message
+        self.mqtt_client.connect(host)
 
+    def _initialize_gpio(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(3, GPIO.FALLING)
+        GPIO.add_event_callback(3, self.button_pressed)
 
-def listening_started():
-    enable_mic()
-    print("Listening has started")
+    def mqtt_on_connect(self, client, userdata, flags, rc):
+        client.subscribe("hermes/#")
 
-
-def listening_stopped():
-    disable_mic()
-    print("Listening has stopped")
-
-
-def hotword_detected():
-    print("Hotword detected")
-    
-
-def process_intent(message):
-    print(message['intent']['intentName'])
-    if message['intent']['intentName'] == 'jzylks:ReadStory':
-        if PLAYER.is_playing():
-            PLAYER.stop()
-            print("Stopping playback")
+    def mqtt_on_message(self, client, userdata, message):
+        topic = message.topic
+        payload = message.payload
+        if topic == "hermes/asr/startListening":
+            self.listening_started()
+        elif topic == "hermes/asr/stopListening":
+            self.listening_stopped()
+        elif re.match("hermes/hotword/.+/detected", topic):
+            self.hotword_detected()
+        elif re.match("hermes/intent/.+", topic):
+            self.process_intent(json.loads(payload))
+        elif topic.startswith("hermes/asr"):
+            print(topic)
         else:
-            story = random.choice(STORIES)
-            print("Starting playback: {}".format(story))
-            PLAYER.set_mrl('file://' + story)
-            PLAYER.play()
+            print(topic)
 
-def button_pressed(event):
-    print('Pressed')
-    message = {'siteId': SITE, 'modelId': MODEL['id'], 'modelVersion': MODEL['version']['nluModel'], 'modelType': 'universal', 'currentSensitivity': 0.5}
-    CLIENT.publish('hermes/hotword/default/detected', json.dumps(message))
+    def listening_started(self):
+        print("Listening has started")
+
+    def listening_stopped(self):
+        print("Listening has stopped")
+
+    def hotword_detected(self):
+        print("Hotword detected")
+        if self.action_sub:
+            self.action_sub.terminate()
+            self.action_sub = None
+
+    def process_intent(self, message):
+        print(message['intent']['intentName'])
+        if message['intent']['intentName'] == 'jzylks:ReadStory':
+            story = random.choice(self.stories)
+            self.action_sub = Popen(['mpg123', story])
+
+    def button_pressed(self, event):
+        print('Pressed')
+        message = {
+            'siteId': self.snips_site, 
+            'modelId': self.snips_model['id'],
+            'modelVersion': self.snips_model['version']['nluModel'],
+            'modelType': 'universal',
+            'currentSensitivity': 0.5
+        }
+        self.client.publish('hermes/hotword/default/detected', json.dumps(message))
+
+    def listen(self):
+        self.mqtt_client.loop_forever()
 
 
-def main():    
-    CLIENT.on_connect = on_connect
-    CLIENT.on_message = on_message
-    CLIENT.connect(HOST)
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(3, GPIO.FALLING)
-    GPIO.add_event_callback(3, button_pressed)
-    
-    CLIENT.loop_forever()
+def main():
+    dinoroar = Dinoroar()
+    dinoroar.listen()
 
 
 if __name__ == '__main__':
